895e1765f1666c7a2a3c957bf1c0cf9307ca8d66
undefinedusername <noah.enger@weatherfordisd.net>
2017-05-02 15:28:51 -0500

    Add files via upload

---
+++ script/openurl.js
@@ -0,0 +1,6 @@
+//
+//Made by Noah Enger
+//
+function openurl(websiteurl) {
+	window.open(websiteurl, "_blank", "toolbar=yes, scrollbars=yes, resizeable=yes, top=500, left=500, width=10000, height=10000");
+}
